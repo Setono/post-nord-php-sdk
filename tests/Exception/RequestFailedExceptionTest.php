f5ebfd9b2b6db2169fce8af06c2b788223e888a3
Joachim Løvgaard <joachim@loevgaard.dk>
2020-08-10 10:01:05 +0200

    Better tests

--- tests/Exception/RequestFailedExceptionTest.php
+++ tests/Exception/RequestFailedExceptionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\PostNord\Exception;
 
+use Nyholm\Psr7\Uri;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -15,13 +16,33 @@ final class RequestFailedExceptionTest extends TestCase
      */
     public function it_returns_correct_values(): void
     {
-        $request = $this->createMock(RequestInterface::class);
-        $response = $this->createMock(ResponseInterface::class);
+        $uri = new Uri('https://example.com/?apikey=secret&other_param=1');
+        $request = $this->prophesize(RequestInterface::class);
+        $request->getUri()->willReturn($uri);
+        $response = $this->prophesize(ResponseInterface::class);
 
-        $exception = new RequestFailedException($request, $response, 200);
+        $exception = new RequestFailedException($request->reveal(), $response->reveal(), 200);
 
-        $this->assertSame($request, $exception->getRequest());
-        $this->assertSame($response, $exception->getResponse());
-        $this->assertSame(200, $exception->getStatusCode());
+        self::assertSame($request->reveal(), $exception->getRequest());
+        self::assertSame($response->reveal(), $exception->getResponse());
+        self::assertSame(200, $exception->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function it_masks_api_key(): void
+    {
+        $uri = new Uri('https://example.com/?apikey=secret&other_param=1');
+        $request = $this->prophesize(RequestInterface::class);
+        $request->getUri()->willReturn($uri);
+        $response = $this->prophesize(ResponseInterface::class);
+
+        $exception = new RequestFailedException($request->reveal(), $response->reveal(), 200);
+
+        self::assertSame(
+            'Request failed with status code 200. Request URI was: https://example.com/?apikey=******&other_param=1',
+            $exception->getMessage()
+        );
     }
 }
